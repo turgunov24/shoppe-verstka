d08fb8c12a14f47fc0d752abf7047b1b9ef4f39e
turgunov24 <learningplatforms.turgunov@gmail.com>
2022-10-28 19:06:51 +0500

    caught four handle

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -1,6 +1,6 @@
 //hooks
 import { useSelector, useDispatch } from "react-redux";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 //pages
 import HomePageSwiper from "./home-page-swiper/home-page-swiper";
